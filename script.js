9d789fd35228495f06d57ee0b3a1e3e77455b019
tomrrees <thomasrrees@gmail.com>
2022-06-17 13:10:45 -0600

    completed CSS portion of project

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
 // Your code here
 const table = document.querySelector(`table`);
-const addRowButton = document.querySelector(`#add-row`).addEventListener("click", function() {makeRow()});
+const addRowButton = document
+  .querySelector(`#add-row`)
+  .addEventListener("click", function () {
+    makeRow();
+  });
 
 function makeRow(input) {
-    const row = document.createElement(`tr`);
-    for (let i = 0; i <20; i++) {
-        const td = document.createElement(`td`);
-        row.appendChild(td);;
-    }
-    table.appendChild(row);
+  const row = document.createElement(`tr`);
+  for (let i = 0; i < 20; i++) {
+    const td = document.createElement(`td`);
+    row.appendChild(td);
+  }
+  table.appendChild(row);
 }
 
 makeRow();
 makeRow();
 makeRow();
 makeRow();
+
+// table.addEventListener("click") {
+//     colorize();
+// }
+
+// function colorize(event) {
+//   let clickedEvent = event.target;
+//   if (clickedEvent.className === "") {
+//     clickedEvent.className = "red";
+//   } else {
+//     clickedEvent.className = "";
+//   }
+// });
+
+function colorize(event) {
+  const clickedEvent = event.target;
+  if (clickedEvent.className === "") {
+    clickedEvent.className = "red";
+  } else {
+    clickedEvent.className = "";
+  }
+}
+
+table.addEventListener("click", function (event) {
+  colorize(event);
+});
