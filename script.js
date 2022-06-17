82bf8533ccc470199067735beee925164fc47cd0
Nik-McKnight <nik.mcknight735@gmail.com>
2022-06-17 11:29:20 -0600

    Creating Rows

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Your code here
+const table = document.querySelector(`table`);
+const addRowButton = document.querySelector(`#add-row`).addEventListener("click", function() {makeRow()});
+
+function makeRow(input) {
+    const row = document.createElement(`tr`);
+    for (let i = 0; i <20; i++) {
+        const td = document.createElement(`td`);
+        row.appendChild(td);;
+    }
+    table.appendChild(row);
+}
+
+makeRow();
+makeRow();
+makeRow();
+makeRow();
